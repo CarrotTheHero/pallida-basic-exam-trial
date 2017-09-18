f609c6c6ac8657eedca0af673e9d8d01d2c76d78
Balazs Bognar <bali.bognar@outlook.com>
2017-09-18 11:59:14 +0200

    task 3 working on not done yet i think

--- dictionary/dictionary/dictionary/Program.cs
+++ dictionary/dictionary/dictionary/Program.cs
@@ -22,27 +22,31 @@ namespace Dictionary
         // Implement this method. It should add the given key-value pair to the the dictionary
         public static void AddWord(string hungarianWord, string englishWord)
         {
-
+            Dictionary.Add("korte", "pear");
         }
 
         // Implement this method. It should remove the key-value pair by the given key from the dictionary
         public static void RemoveWord(string hungarianWord)
         {
-
+            Dictionary.Remove("fa");
         }
 
         // Implement a method which works as a translator from Hungarian to English
         // Example: you give it a parameter "alma" and it's output is "tree"
         public static string TranslateToEnglish(string hungarian)
         {
-
+            Console.Write("Write in like alma, korte, fa: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(Dictionary[input]);
         }
 
         // Implement a method which works as a translator from English to Hungarian
         // Example: you give it a parameter "apple" and it's output is "alma"
         public static string TranslateToHungarian(string english)
         {
-
+            Console.Write("Write in like apple, pear, tree: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(Dictionary[input]);
         }
     }
 }
